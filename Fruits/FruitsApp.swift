890420b38948393a716d6b5352aff10ca09bd58a
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-05-04 12:49:10 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Burak Özdemir on 4.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
